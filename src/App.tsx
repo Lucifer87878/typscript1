93a329df3cccc64ae40ef908c61b2f2c43cd84c9
Goran P <nallenrockar123@gmail.com>
2024-03-04 13:26:16 +0100

    ny funktion

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import Counter from "./Counter";
 import Header from "./Header";
+import TextToAlert from "./TextToAlert";
 
 function App() {
   return(
@@ -8,6 +9,7 @@ function App() {
   Rubril & <span>ngt mer?</span>
   </Header>
   <Counter />
+  <TextToAlert />
   </>
   )
 }
